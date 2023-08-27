ff22bdaa8863c926068690879eebace4d5198287
Saurabh Purohit <saurabhpurohit@Saurabhs-MacBook-Air.local>
2023-08-27 10:15:51 +0530

    Create restaurant API

---
+++ configs/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DB_URL: "mongodb://localhost:27017/find_my_restaurant",
+};
---
+++ controllers/restaurant.controller.js
@@ -0,0 +1,47 @@
+const Restaurant = require('../models/restaurant.model');
+
+exports.createRestaurant = async (req, res) => {
+    try {
+        if (!req.body.name ||
+            !req.body.description ||
+            !req.body.category ||
+            !req.body.imageURL ||
+            !req.body.location ||
+            !req.body.phone ||
+            !req.body.rating) {
+            return res.status(500).send({
+                message: `Content cannot be empty`
+            });
+        }
+
+        const restaurantObject = {
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            imageURL: req.body.imageURL,
+            location: req.body.location,
+            phone: req.body.phone,
+            rating: req.body.rating,
+        };
+        const restaurantCreated = await Restaurant.create(restaurantObject);
+        const restaurant = {
+            name: restaurantCreated.name,
+            description: restaurantCreated.description,
+            category: restaurantCreated.category,
+            imageURL: restaurantCreated.imageURL,
+            location: restaurantCreated.location,
+            phone: restaurantCreated.phone,
+            rating: restaurantCreated.rating,
+            _id: restaurantCreated._id,
+            createdAt: restaurantCreated.createdAt,
+            updatedAt: restaurantCreated.updatedAt,
+            __v: restaurantCreated.__v,
+        };
+        return res.status(200).send(restaurant);
+    } catch (error) {
+        console.log(`Error while creating a new restaurant: ${error}`);
+        return res.status(500).send({
+            message: "Error while creating a new Restaurant",
+        });
+    };
+};
